fa3c50915d41738d521c752ed65726f30184c347
unknown <spencerm@comcast.net>
2021-05-04 17:56:09 -0400

    Nullable collection on book

---
+++ backend/book-tracker/Migrations/20210504215538_NullableCollection.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace book_tracker.Migrations
+{
+    public partial class NullableCollection : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Collections_CollectionId",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CollectionId",
+                table: "Books",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Collections_CollectionId",
+                table: "Books",
+                column: "CollectionId",
+                principalTable: "Collections",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_Collections_CollectionId",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CollectionId",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_Collections_CollectionId",
+                table: "Books",
+                column: "CollectionId",
+                principalTable: "Collections",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
