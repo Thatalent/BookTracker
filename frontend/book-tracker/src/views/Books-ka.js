d1fefdf92b41b2e490a0f7e9fee31dd5fa350cdf
Kashif Ahmad <ahmadk72@gmail.com>
2021-05-08 13:59:09 -0400

    Kashif changes for sorting and column/row size

--- frontend/book-tracker/src/views/Books-ka.js
+++ frontend/book-tracker/src/views/Books-ka.js
@@ -7,20 +7,42 @@ import {
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 import { getConfig } from "../config";
 import Loading from "../components/Loading";
-import {Column, Table, SortDirection } from 'react-virtualized';
-//import {createTableMultiSort, Column, Table} from 'react-virtualized';
+import {Column, Table, AutoSizer, CellMeasurerCache, CellMeasurer, SortDirection, tableSettings } from 'react-virtualized';
 
 
 export class BooksComponent extends React.Component {
 
  constructor(props) {
     super(props);
-    this.state = { input: "", books: [], sortedBooks: [], sortBy: "author_name, publish_year, subject" };
+    this.state = { input: "", books: [], sortedBooks: [], sortBy: "author_name, subject, publish_year" };
     this.headerRenderer = this.headerRenderer.bind(this);
     this.sort = this.sort.bind(this);
+    this._cache = new CellMeasurerCache({
+      fixedWidth: true,
+      minHeight: 40
+    });
     /*this.getHeader = this.getHeader.bind(this);
     this.getRowsData = this.getRowsData.bind(this);
     this.getKeys = this.getKeys.bind(this);*/
+  };
+  
+  _wrappingCellRenderer = ({...sortedBooks}) => {
+    return (
+      <CellMeasurer
+        cache={this._cache}
+        columnIndex={sortedBooks.columnIndex}
+        key={sortedBooks.dataKey}
+        parent={sortedBooks.parent}
+        rowIndex={sortedBooks.rowIndex}>
+        <div
+          className={"tableColumn"}
+          style={{
+            whiteSpace: 'normal',
+          }}>
+          {sortedBooks.cellData}
+        </div>
+      </CellMeasurer>
+    );
   };
 
   coverPrepend = 'https://covers.openlibrary.org/b/olid/';
@@ -86,6 +108,18 @@ export class BooksComponent extends React.Component {
 
   render(){
     let emptyBooks = !this.state.sortedBooks || this.state.sortedBooks.length < 1;
+    var tableSettings =  {
+      columns: [
+        { dataKey: 'isbn', label: 'ISBN', width: 100, flexGrow:1 },
+        { dataKey: 'title', label: 'Title', width: 100, flexGrow: 1 },
+        { dataKey: 'author_name', label: 'Author Name', width: 150, flexGrow: 1 },
+        { dataKey: 'cover_edition_key', label: 'Cover', width: 150, flexGrow: 1 },
+        { dataKey: 'subject', label: 'Genre', width: 150, flexGrow: 1 },
+        { dataKey: 'publish_year', label: 'Year Published', width: 450, flexGrow: 1 },
+        { dataKey: 'publisher', label: 'Publisher', width: 150, flexGrow: 1 },
+        { dataKey: 'read', label: 'Read', width: 150, flexGrow: 1 }
+      ]
+    }
   return (
     <>
       <div className="mb-5">
@@ -109,9 +143,12 @@ export class BooksComponent extends React.Component {
             </InputGroupAddon>
         </InputGroup>
       </div>
+  <AutoSizer>
+        {({ height, width }) => (
       <Table
-        width={1000}
-        height={500}
+      deferredMeasurementCache={this._cache}
+        width={width}
+        height={height}
         headerHeight={20}
         rowHeight={100}
         rowCount={this.state.sortedBooks.length}
@@ -121,21 +158,22 @@ export class BooksComponent extends React.Component {
         sort={this.state.sort}
         sortBy={this.state.sortBy}
         sortDirection={this.state.sortDirection}>
-        <Column width={100} label="Select" dataKey="isbn" onClick={this.selectBook} >
-          <Input type="checkbox" />
-        </Column>
-        <Column width={200} label="Title" dataKey="title"  />
-        <Column width={200} label="Author" dataKey="author_name" headerRenderer={this.headerRenderer} />
-        <Column width={200} label="Cover" dataKey="cover_edition_key" cellRenderer={this.ImageCell}  />
-        <Column width={100} label="Genre" dataKey="subject"  />
-        <Column width={300} label="Year Published" dataKey="publish_year" headerRenderer={this.headerRenderer} />
-        <Column width={300} label="Publisher" dataKey="publisher" headerRenderer={this.headerRenderer} />
-        <Column width={100} label="read" dataKey="read"  />
-      </Table>
+        {tableSettings.columns.map((column, key) => (
+        <Column
+                  width={150}
+                  label={column.label}
+                  dataKey={column.dataKey}
+                  key={key}
+                  cellRenderer={this._wrappingCellRenderer}
+                  />
+        ))}
+       </Table>
+     )}
+    </AutoSizer>
     </>
-  );
+    );
   }
-};
+}
 
 export default withAuthenticationRequired(BooksComponent, {
   onRedirecting: () => <Loading />,
